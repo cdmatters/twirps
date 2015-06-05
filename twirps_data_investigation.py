14b171690e5a7e3a7f6fcd6de2f8648fe37f4df6
Eric Hambro <eric.hambro@gmail.com>
2015-06-05 12:51:08 +0100

    auxiliary kmeans functions written

--- twirps_data_investigation.py
+++ twirps_data_investigation.py
@@ -1,5 +1,8 @@
 import numpy as np 
+import scipy as sp
 from sklearn.feature_extraction import DictVectorizer
+from sklearn.preprocessing import normalize 
+
 import json, sys 
 
 np.set_printoptions(threshold=1e9)
@@ -22,23 +25,66 @@ when cluster and centroids not changing finish algo.
 def naive_clustering(filename):
     """perform kmeans clustering on a single assimilated frequency json file. 
 output to .csv"""
-
+    
+    k = 4
 
     dv = DictVectorizer(sparse=True)
-
-    MPnames = []
-    MPdata = []
-
+    
     with open(filename, 'r') as f:
         raw = json.load(f)
-
+        MPnames, MPrawdata = [],[]
+        
         for MP in raw:
             MPnames.append(MP)
-            MPdata.append(raw[MP])
+            MPrawdata.append(raw[MP])
 
-    sparse_array = dv.fit_transform(MPdata)
+    sparse_array = dv.fit_transform(MPrawdata)
+    data_points = sparse_array #should this be normalized? hashtag ratios or absolute?
+    
+    mp_no , dim = data_points.get_shape()
 
-    print sparse_array.get_shape()
+    initial_centroids = normalize(sp.sparse.rand(dim, k , density=0.1), axis=0)
+    centroids = initial_centroids.tocsc()
+
+
+    distances = kmeans_find_distance(data_points, centroids)
+    clusters = kmeans_find_cluster(distances)
+    centroids = kmeans_find_centroid(data_points, clusters, k)
+
+
+def kmeans_find_distance(M,C):
+    """Return a matrix with distance of each datapoint (matrix M) from each centroid (matrix C)"""
+    return M.dot(C)
+    
+
+def kmeans_find_cluster(D):
+    """Return a list assigning each twirp (a row in data matrix), to a cluster"""
+    i = 0
+    cluster = []
+    while i < D.get_shape()[0]:
+        row = D.getrow(i).todense()
+        cluster.append(np.argmax(row))  
+        i+=1
+    return cluster
+
+def kmeans_find_centroid(M, clusters, k):
+    """Return a matrix of centroids (as columns)"""
+    i = 0
+    clust_indices = []
+    centroid_list = []
+    
+    while i< k:
+        clust_array = np.array(clusters)
+        clust_indices = np.where( clust_array == i )[0]
+
+        A = M[clust_indices] #matrix of clusters
+        centroid_list.append(sp.sparse.csr_matrix(A.mean(axis=0)))
+        i+=1
+    print centroid_list
+
+    C = sp.sparse.vstack(centroid_list).transpose()
+    return C
+
 
         
         
