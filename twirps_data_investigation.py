dcdec38049e2bdfcb714f396ce6b7ce67ca32f53
Eric Hambro <eric.hambro@gmail.com>
2015-06-08 19:30:44 +0100

    form of variance now calculated, best results taken, printed to specific filename

--- twirps_data_investigation.py
+++ twirps_data_investigation.py
@@ -3,7 +3,7 @@ import scipy as sp
 from sklearn.feature_extraction import DictVectorizer
 from sklearn.preprocessing import normalize 
 
-import json, sys 
+import json, sys , sets
 
 np.set_printoptions(threshold=1e9)
 
@@ -19,14 +19,14 @@ to find mean of cluster:
     - take rows in cluster (normalized) & generate from this
 
 when cluster and centroids not changing finish algo.
+
+finally: run multiple times, and find ratio of sumsquare distances
 """
 
 
-def naive_clustering(filename):
+def naive_clustering(filename, k):
     """perform kmeans clustering on a single assimilated frequency json file. 
 output to json for d3 visual exploration"""
-    
-    k = 1
 
     dv = DictVectorizer(sparse=True)
     
@@ -39,7 +39,7 @@ output to json for d3 visual exploration"""
             MPrawdata.append(raw[MP])
 
     sparse_array = dv.fit_transform(MPrawdata)
-    data_points = sparse_array #should this be normalized? hashtag ratios or absolute?
+    data_points = normalize(sparse_array, axis=1) #should this be normalized? hashtag ratios or absolute?
     
     mp_no , dim = data_points.get_shape()
 
@@ -58,11 +58,12 @@ output to json for d3 visual exploration"""
 
         if old_clusters == clusters: #no convergence with centroids?
             count +=1
-
-    #note: no averages done yet
-    result = { MP:{'cluster': clusters[i]} for i, MP in enumerate(MPnames) }
     
-    print_json(result)
+    variance_ratio = find_variance_ratio(distances, clusters, data_points)
+
+    result = { MP:{'cluster': clusters[i]} for i, MP in enumerate(MPnames) }
+    return (variance_ratio, result)
+    #print_json(result)
 
 
 def kmeans_find_distance(M,C):
@@ -94,13 +95,31 @@ def kmeans_find_centroid(M, clusters, k):
         centroid_list.append(sp.sparse.csr_matrix(A.mean(axis=0)))
         i+=1
     
-    print centroid_list
+    #print centroid_list
 
     C = sp.sparse.vstack(centroid_list).transpose()
     C = np.round(C, decimals=5)
     return C
 
-def print_json(results):
+def kmeans_find_variance_ratio(distances, clusters, M):
+    """Return the sum of sumsquares of cosine distances within clusters, 
+compared to overall sumsquare of cosine distances"""
+    internal_variance = 0 
+    
+    for i, c in enumerate(clusters):
+        internal_variance += (distances[i, c])**2
+
+    #find total variance compared to mean by running for 1 centroid   
+    single_centroid = kmeans_find_centroid(M, [0]*len(clusters), 1 )
+    single_dist = kmeans_find_distance(M, single_centroid)
+
+    total_var = sum(map(lambda x:x**2, single_dist))
+    
+    print internal_variance/total_var[0,0]
+    return internal_variance/total_var[0,0]
+
+
+def print_json(results, title):
     "add archipelago & twirp info to results, and write full results to JSON"
     with open('data_analysis_vis/basic_info.json', 'r+') as b:
         basix = json.load(b)
@@ -109,15 +128,44 @@ def print_json(results):
         if data['handle'] in results.keys():
             data.update(results[data['handle']])
 
-    with open('data_analysis_vis/kmeans_naive.json', 'w+') as f:
+    with open('data_analysis_vis/kmeans_%s.json'%title, 'w+') as f:
         f.write(json.dumps(basix))
 
+# def identify_clusters(setdictA, setdictB):
+#     """return correspondingly equivalent cluster numbers between two different clustering results"""
+#     mapping = {}
+#     #print type(setdictA), type(setdictA[1])
+#     for setA in setdictA:
+#         overlap = []
+#         correspondence = {}
+#         for setB in setdictB:
+#             if len(setdictB[setB] & setdictA[setA]) > len(overlap):
+#                 overlap = (setdictB[setB] & setdictA[setA])
+#                 correspondence = {setA: setB}
+#                 #print correspondence, overlap
+#         mapping.update(correspondence)
+#     return mapping
+
+
+def run_multiple_clusterings(filename, N, k, title):
+    """run kmeans on given file, N times and print averages to JSON"""
+    best_ratio = 0
+    for i in range(N):
+        print i, 
+        var_ratio, results = naive_clustering(filename, k)
+        if var_ratio > best_ratio:
+            best_ratio = var_ratio
+            final_results = results
+
+    title = title+ str(best_ratio).replace('.','_')
+    print_json(final_results, title)
+    print best_ratio
 
 
 
 if __name__ == '__main__':
     if sys.argv[1] == 'naive_cluster':
-        naive_clustering(sys.argv[2])
+        run_multiple_clusterings(sys.argv[2], 50, int(sys.argv[3]), sys.argv[4])
     else:
         print 'arguments error'
 
