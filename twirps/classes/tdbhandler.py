6391b2d09b292285158433bdeba1610e9d61915c
Eric Hambro <eric.hambro@gmail.com>
2016-03-05 19:39:10 +0000

    added to postgress get oldest and newest tweet requests

--- twirps/classes/tdbhandler.py
+++ twirps/classes/tdbhandler.py
@@ -167,6 +167,56 @@ class TDBHandler(object):
             for m in t.mentions:
                 cur.execute(add_entity_sql, (t.tweetid, t.userid, 'mention', m[1], m[0]))
 
+    def get_oldest_tweets_stored_from_mps(self):
+        with psycopg2.connect(self.pg_database) as connection:
+            cur = connection.cursor()
+            cur.execute('''SELECT COUNT(tweet.TweetID), MIN(tweet.TweetID), 
+                                twirp.UserID, twirp.Name, twirp.Handle, twirp.TweetCount 
+                            FROM TwirpData AS twirp
+                            LEFT JOIN  TweetData AS tweet
+                            ON twirp.UserID=tweet.UserID
+                            GROUP BY twirp.UserID
+                            ORDER BY twirp.Name
+                            ''')
+    
+        return [ 
+                    {
+                        "name": r[3] ,
+                        "handle": r[4] , 
+                        "no_tweets": r[5],
+                        "oldest": r[1],
+                        'no_collected': r[0], 
+                        "u_id": r[2]
+
+                    } for r in cur.fetchall()
+                ]
+
+    def get_newest_tweets_from_mps(self):
+        with psycopg2.connect(self.pg_database) as connection:
+            cur = connection.cursor()
+            cur.execute('''SELECT COUNT(tweet.TweetID), MAX(tweet.TweetID), 
+                                twirp.UserID, twirp.Name, twirp.Handle, twirp.TweetCount 
+                            FROM TwirpData AS twirp
+                            LEFT JOIN  TweetData AS tweet
+                            ON twirp.UserID=tweet.UserID
+                            GROUP BY twirp.UserID
+                            ORDER BY twirp.Name
+                            ''')
+        return [ 
+                    {
+                        "name": r[3] ,
+                        "handle": r[4] , 
+                        "no_tweets": r[5],
+                        "newest": r[1],
+                        'no_collected': r[0], 
+                        "u_id": r[2]
+                    } for r in cur.fetchall()
+                ]
+
+
+
+
+
 
     def get_stored_mps_names(self):
         with psycopg2.connect(self.pg_database) as connection:
@@ -183,27 +233,27 @@ class TDBHandler(object):
             os.remove(self.db_name)
         self.create_twirpy_db()
 
-    def create_twirpy_db(self):
-        '''Creates a database with tables for TweetData and TwirpData'''
+    # def create_twirpy_db(self):
+    #     '''Creates a database with tables for TweetData and TwirpData'''
 
-        with sqlite3.connect(self.db_name) as connection:
-            cur = connection.cursor()
-            cur.execute('CREATE TABLE TweetData (UserID Number, UserHandle Text, FavouriteCount Number, \
-                                                RetweetCount Number, Content Text, Retweet Text, \
-                                                CreatedDate Text, TwitterID Number UNIQUE)')
-            cur.execute('CREATE TABLE TwirpData (UserID Number UNIQUE, UserName Text, Name Text, Handle Text, \
-                                                FollowersCount Number, FriendsCount Number,\
-                                                TweetCount Number, RetweetCount Number, \
-                                                BeenRetweeted Number, FavouriteHashtag Text, \
-                                                HashtagCount Number, OfficialId Number,\
-                                                Subscribed Number default 0)')
-            cur.execute('CREATE TABLE TweetEntities (TweetID Number, UserID Number,\
-                                                EntityType Text, Entity Text, ToUser Number,\
-                                                UrlBase Text, UNIQUE(TweetID, UserID, EntityType, Entity) )')
+    #     with sqlite3.connect(self.db_name) as connection:
+    #         cur = connection.cursor()
+    #         cur.execute('CREATE TABLE TweetData (UserID Number, UserHandle Text, FavouriteCount Number, \
+    #                                             RetweetCount Number, Content Text, Retweet Text, \
+    #                                             CreatedDate Text, TwitterID Number UNIQUE)')
+    #         cur.execute('CREATE TABLE TwirpData (UserID Number UNIQUE, UserName Text, Name Text, Handle Text, \
+    #                                             FollowersCount Number, FriendsCount Number,\
+    #                                             TweetCount Number, RetweetCount Number, \
+    #                                             BeenRetweeted Number, FavouriteHashtag Text, \
+    #                                             HashtagCount Number, OfficialId Number,\
+    #                                             Subscribed Number default 0)')
+    #         cur.execute('CREATE TABLE TweetEntities (TweetID Number, UserID Number,\
+    #                                             EntityType Text, Entity Text, ToUser Number,\
+    #                                             UrlBase Text, UNIQUE(TweetID, UserID, EntityType, Entity) )')
 
-            cur.execute('CREATE INDEX UserIDIndex ON TweetData (UserID)')
-            cur.execute('CREATE INDEX UserIDEntityIndex ON TweetEntities (UserID)')
-        LOGGER.debug("Created twirpy database, name: %s" % self.db_name )
+    #         cur.execute('CREATE INDEX UserIDIndex ON TweetData (UserID)')
+    #         cur.execute('CREATE INDEX UserIDEntityIndex ON TweetEntities (UserID)')
+    #     LOGGER.debug("Created twirpy database, name: %s" % self.db_name )
 
     # def _add_Twirp_to_database(self, Twirp):
 
@@ -243,7 +293,7 @@ class TDBHandler(object):
 
     #     return [ name[0] for name in cur.fetchall() ]
 
-    def get_oldest_tweets_stored_from_mps(self):
+    def _get_oldest_tweets_stored_from_mps(self):
         with sqlite3.connect(self.db_name) as connection:
             cur = connection.cursor()
             cur.execute('''SELECT COUNT(tweet.TwitterID), MIN(tweet.TwitterID), 
@@ -267,7 +317,7 @@ class TDBHandler(object):
                     } for r in cur.fetchall()
                 ]
 
-    def get_newest_tweets_from_mps(self):
+    def _get_newest_tweets_from_mps(self):
         with sqlite3.connect(self.db_name) as connection:
             cur = connection.cursor()
             cur.execute('''SELECT COUNT(tweet.TwitterID), MAX(tweet.TwitterID), 
